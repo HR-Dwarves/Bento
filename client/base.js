b56fd9405b3d0ccce42f452373da064e944a0cf2
Michael Tutt <tutt.michael@gmail.com>
2017-02-15 21:46:26 -0800

    remove config key reference

--- client/base.js
+++ client/base.js
@@ -3,12 +3,6 @@
 import firebase from 'firebase';
 import config from './config/config';
 
-// const config = {
-//   apiKey: 'AIzaSyA2Vq9okaPvwNahEVH0s-KgyJ2fBattPOw',
-//   authDomain: 'dashboardapp-3d3c7.firebaseapp.com',
-//   databaseURL: 'https://dashboardapp-3d3c7.firebaseio.com'
-// };
-
 firebase.initializeApp(config);
 const database = firebase.database();
 
