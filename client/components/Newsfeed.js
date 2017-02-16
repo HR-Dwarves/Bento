f0505b748b4fafdc89bfd2b78cb2306ca5264956
joeylaguna <elagunaj@gmail.com>
2017-02-15 20:11:19 -0800

    Do ajax call to get most recent 5 hacker news stories inside Newsfeed

--- client/components/Newsfeed.js
+++ client/components/Newsfeed.js
@@ -1,6 +1,35 @@
 import React from 'react';
 
-const Test2 = React.createClass({
+const Newsfeed = React.createClass({
+
+  componentWillMount(){
+    let feed = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+    let itemIdArray = [];
+    $.ajax({
+      url: feed,
+      type: 'GET',
+      dataType: 'json', // added data type
+      success: function(res) {
+  	    for(var i = 0; i < 5; i++) {
+  	      itemIdArray.push(res[i]);
+   	    }
+        for(var i = 0; i < itemIdArray.length; i++) {
+          $.ajax({
+            url: 'https://hacker-news.firebaseio.com/v0/item/'+ itemIdArray[i] +'.json?print=pretty',
+            type: 'GET',
+            dataType: 'json',
+            success: function(res) {
+              for(var i = 0; i < itemIdArray.length; i++) {
+                itemIdArray[i] = res;
+                console.log(itemIdArray[i].title);	
+              }
+            },
+          });
+        }   
+      },
+    });
+  },
+
   render() {
     return (
       <div>
@@ -10,4 +39,4 @@ const Test2 = React.createClass({
   }
 });
 
-export default Test2;
+export default Newsfeed;
