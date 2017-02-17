c04f8789e6d27467e92fdcc7497d14b8617ed589
Michael Tutt <tutt.michael@gmail.com>
2017-02-16 19:29:55 -0800

    Add font awesome to index.html

--- client/base.js
+++ client/base.js
@@ -7,14 +7,3 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 export default database;
-
-// REBASE NPM PACKAGE
-// import Rebase from 're-base';
-
-// const base = Rebase.createClass({
-//   apiKey: 'AIzaSyA2Vq9okaPvwNahEVH0s-KgyJ2fBattPOw',
-//   authDomain: 'dashboardapp-3d3c7.firebaseapp.com',
-//   databaseURL: 'https://dashboardapp-3d3c7.firebaseio.com',
-// });
-
-// export default base;
