3518388a0b07c4abeac9080c5a6fe481b7822ed1
Benson Ninh <bensonninh@gmail.com>
2017-02-17 15:33:09 -0800

    remove console logs

--- client/components/NewsItem/NewsItem.js
+++ client/components/NewsItem/NewsItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NewsItem = React.createClass({
   render(){
   	const item = this.props.newsItem;
-  	console.log(item.url);
+  	// console.log(item.url);
   	return(
   	  <div>
   	    <a href={item.url ? item.url : 'https://news.ycombinator.com/item?id=' + item.id} target="_blank">{item.title}</a>
