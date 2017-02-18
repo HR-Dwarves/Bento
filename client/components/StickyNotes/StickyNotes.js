795229f9823596b6124ed56773dd5898b0297d1c
Benson Ninh <bensonninh@gmail.com>
2017-02-17 16:55:24 -0800

    initialize sticky note component

---
+++ client/components/StickyNotes/StickyNotes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './StickyNotes.css';
+
+class StickyNotes extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        StickyNote
+      </div>
+    )
+  }
+}
+
+export default StickyNotes;
