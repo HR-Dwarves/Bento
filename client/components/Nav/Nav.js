6b78b7ffd5b4d89af270907a0690cb09ad004a0a
joeylaguna <elagunaj@gmail.com>
2017-02-20 11:47:32 -0800

    Refactor Nav class

--- client/components/Nav/Nav.js
+++ client/components/Nav/Nav.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router';
 import styles from './Nav.css'
+import Dashboard from './../Dashboard/Dashboard'
 
-const Nav = React.createClass({
+class Nav extends React.Component {
+  constructor() {
+    super();
+    this.state = {isModalOpen: false}
+  }
 
   render() {
     let mainNavPanelCSS = `${styles.mainNavPanel} is-clearfix`
     let pageTitleCSS = `${styles.pageTitle} has-text-centered`
-    
-    return (
+
+    return(
       <div className={mainNavPanelCSS}>
         <Link to="/">
           <div className={pageTitleCSS}>dashboard</div>
         </Link>
+        <button className="button is-primary modal-button"><i className="fa fa-cog" aria-hidden="true"></i></button>
       </div>
     )
   }
-});
+}
+
+// const Nav = React.createClass({
+
+//   render() {
+//     let mainNavPanelCSS = `${styles.mainNavPanel} is-clearfix`
+//     let pageTitleCSS = `${styles.pageTitle} has-text-centered`
+    
+//     return (
+//       <div className={mainNavPanelCSS}>
+//         <Link to="/">
+//           <div className={pageTitleCSS}>dashboard</div>
+//         </Link>
+//         <button className="button is-primary modal-button"><i className="fa fa-cog" aria-hidden="true"></i></button>
+//       </div>
+//     )
+//   }
+// });
 
 export default Nav;
