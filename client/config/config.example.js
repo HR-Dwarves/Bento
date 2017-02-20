eafad923e09d73e782e023b5a83891de90402509
Benson Ninh <bensonninh@gmail.com>
2017-02-20 12:09:33 -0800

    add to config example js

--- client/config/config.example.js
+++ client/config/config.example.js
@@ -1,7 +1,9 @@
 const config = {
   apiKey: 'ADD KEY HERE!',
   authDomain: 'ADD DOMAIN HERE!',
-  databaseURL: 'ADD URL HERE!'
+  databaseURL: 'ADD URL HERE!',
+  openWeatherMapAPIKey: 'API KEY HERE',
+  googleApiKey: 'GEO LOCATION API KEY HERE'
 };
 
 export default config;
