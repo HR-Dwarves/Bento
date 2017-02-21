58c81ba16db408d3b6ef73f3957fecde77d2836b
Michael Tutt <tutt.michael@gmail.com>
2017-02-21 12:21:08 -0800

    Add module wrappe with delete button

---
+++ client/components/ModuleWrapper/ModuleWrapper.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import database from '../../base';
+import styles from './ModuleWrapper.css';
+import moduleList from '../../data/moduleList';
+
+class ModuleWrapper extends React.Component {
+  constructor() {
+    super();
+    this.handleDelete = this.handleDelete.bind(this);
+
+  }
+
+
+  handleDelete() {
+    let db_key = this.props.db_key;
+    // let user = this.props.user.uid;
+
+    this.props.deleteModule(db_key);
+  }
+
+  render() {
+    let type = this.props.type;
+    let component = moduleList[type];
+    let deleteButton = `${styles.delete} delete`;
+    let wrapperStyle = `${styles.wrapper}`;
+
+    return (
+      <div className={wrapperStyle}>
+          <button className={deleteButton} onClick={this.handleDelete}></button>
+        {React.createElement(component, {...this.props})}
+      </div>
+    )
+  }
+}
+
+export default ModuleWrapper;
