a34f9ab9d53e4cfa6cf76ab5ff383387381b5d9b
Benson Ninh <bensonninh@gmail.com>
2017-02-20 18:36:50 -0800

    add event listeners to text areas to update database

--- client/components/StickyNotes/StickyNotes.js
+++ client/components/StickyNotes/StickyNotes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import database from '../../base';
 import Draggable from 'react-draggable';
 
 import styles from './StickyNotes.css';
@@ -6,10 +7,11 @@ import styles from './StickyNotes.css';
 class StickyNotes extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.handleAddSticky = this.handleAddSticky.bind(this);
     this.handleDeleteSticky = this.handleDeleteSticky.bind(this);
-    this.handleStickyEntry = this.handleStickyEntry.bind(this);
+    this.handleStickyHeaderEntry = this.handleStickyHeaderEntry.bind(this);
+    this.handleStickyBodyEntry = this.handleStickyBodyEntry.bind(this);
   }
 
   handleAddSticky() {
@@ -20,20 +22,39 @@ class StickyNotes extends React.Component {
     console.log('DELETE THIS STICKY');
   }
 
-  handleStickyHeaderEntry() {
-    console.log($('.sticky-header').val());
+  handleStickyHeaderEntry(event) {
+    const db_key = this.props.db_key;
+    const target = 'headerText';
+    const db_ref = database.ref(`/testUser/modules/${db_key}/${target}`);
+    let newText = event.target.value;    
+    // db_ref.set(newText);
+    this.props.updateText(target, newText, db_key);
   }
 
-  handleStickyBodyEntry() {
-    console.log($('.sticky-content').val());
+  handleStickyBodyEntry(event) {
+    const db_key = this.props.db_key;
+    const target = 'bodyText';
+    const db_ref = database.ref(`/testUser/modules/${db_key}/${target}`);
+    let newText = event.target.value;
+    this.props.updateText(target, newText, db_key);
   }
 
   render() {
+    let dashboard = this.props.dashboard;
+    let db_key = this.props.db_key;
+    let note = dashboard.modules[db_key];
+
     return(
       <div className=''>
           <div className='card'>
             <header className='card-header'>
-              <textarea className='card-header-title sticky-header' cols='30' maxLength='30' onChange={this.handleStickyHeaderEntry}></textarea>
+              <textarea className='card-header-title sticky-header' 
+                        cols='30' 
+                        maxLength='30' 
+                        onChange={this.handleStickyHeaderEntry}
+                        value={note.headerText}
+                        >
+              </textarea>
               <div className="card-header-icon">
                 <span>
                   <i className='fa fa-plus' aria-hidden='true' onClick={this.handleAddSticky}></i>
@@ -44,7 +65,14 @@ class StickyNotes extends React.Component {
               </div>
             </header>
             <div className='card-content'>
-              <textarea className='sticky-content' maxLength='600' rows='10' cols='60' onChange={this.handleStickyBodyEntry}></textarea>
+              <textarea className='sticky-content' 
+                        maxLength='600' 
+                        rows='10' 
+                        cols='60'
+                        onChange={this.handleStickyBodyEntry}
+                        value={note.bodyText}
+                        >
+              </textarea>
             </div>
           </div>
       </div>
