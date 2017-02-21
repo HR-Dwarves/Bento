ded9f7e5210fc6195c0c23e8f15689c3c38a5815
Michael Tutt <tutt.michael@gmail.com>
2017-02-20 16:02:33 -0800

    Add login component and CSS files

---
+++ client/components/Login/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './Login.css';
+import database from '../../base';
+
+
+class Login extends React.Component {
+  constructor() {
+    super();
+  }
+
+  handleLogin() {
+
+  }
+
+  authenticate(provider){
+    database.authWithOAuthPopup(provider, this.authHandler);
+  }
+
+  authHandler() {
+
+  }
+
+  render() {
+    return (
+      <nav className="login">
+        <div>LOGIN</div>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="google" onClick={() => this.authenticate('google')}>Log In with Google</button>
+        <button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
+        <button className="facebook" onClick={() => this.authenticate('facebook')} >Log In with Facebook</button>
+        <button className="twitter" onClick={() => this.authenticate('twitter')} >Log In with Twitter</button>
+      </nav>
+    );
+  }
+}
+
+export default Login;
