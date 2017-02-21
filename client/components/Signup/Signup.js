ff54891b9fcc647be5781e572481752e0ec8621c
Michael Tutt <tutt.michael@gmail.com>
2017-02-20 19:10:57 -0800

    Add firebase auth to signup page

--- client/components/Signup/Signup.js
+++ client/components/Signup/Signup.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import firebase from 'firebase';
 import styles from './Signup.css';
 import database from '../../base';
+import authProviders from '../../authProviders';
 
 
 class Signup extends React.Component {
   constructor() {
     super();
+    this.state = {
+      user: null
+    }
 
     this.authHandler = this.authHandler.bind(this);
+    this.authenticate = this.authenticate.bind(this);
   }
 
   handleSignup() {
@@ -15,22 +21,43 @@ class Signup extends React.Component {
   }
 
   authenticate(provider){
-    database.authWithOAuthPopup(provider, this.authHandler);
+    var context = this;
+    var authProvider = authProviders[provider];
+    firebase.auth().signInWithPopup(authProvider)
+    .then(function(result) {
+      context.setState({user: result});
+    })
+    .catch(function(err) {
+      console.error(err);
+    })
+  }
+
+  authWithGoogle() {
+
   }
 
   authHandler(err, authData) {
-
+    if (err) {
+      console.error(err);
+    }
+    console.log('AUTH DATA');
+    console.log(authData);
   }
 
   render() {
-    return (<nav className="signup">
-      <div>SIGNUP</div>
-      <p>Sign in to manage your store's inventory</p>
-      <button className="google" onClick={() => this.authenticate('google')}>Log In with Google</button>
-      <button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
-      <button className="facebook" onClick={() => this.authenticate('facebook')} >Log In with Facebook</button>
-      <button className="twitter" onClick={() => this.authenticate('twitter')} >Log In with Twitter</button>
-    </nav>
+    return (
+      <nav className={styles.signup}>
+        <div>
+          <span>SIGNUP</span>
+        </div>
+        <div>
+          <p>Sign in to manage your store's inventory</p>
+          <button className="google button" onClick={() => this.authenticate('google')}>Log In with Google</button>
+          <button className="github button" onClick={() => this.authenticate('github')}>Log In with Github</button>
+          <button className="facebook button" onClick={() => this.authenticate('facebook')} >Log In with Facebook</button>
+          <button className="twitter button" onClick={() => this.authenticate('twitter')} >Log In with Twitter</button>
+        </div>
+      </nav>
     );
   }
 }
