92eb8c8d767b57339b21de8afb9332b4097bfae6
joeylaguna <elagunaj@gmail.com>
2017-02-23 12:54:44 -0800

    Implement collapse function in module wrapper. Make newsfeed collapse

--- client/components/ModuleWrapper/ModuleWrapper.js
+++ client/components/ModuleWrapper/ModuleWrapper.js
@@ -8,7 +8,8 @@ class ModuleWrapper extends React.Component {
   constructor() {
     super();
     this.handleDelete = this.handleDelete.bind(this);
-
+    this.state = {collapsed: false}
+    this.handleCollapse = this.handleCollapse.bind(this);
   }
 
 
@@ -19,8 +20,16 @@ class ModuleWrapper extends React.Component {
     this.props.deleteModule(db_key, user);
   }
 
+  handleCollapse() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
     let type = this.props.type;
+    let collapsed = this.state;
+    let handleCollapseFunction = this.handleCollapse;
     let component = moduleList[type];
     // let deleteButton = `${styles.delete} delete`;
     let deleteButton = `${styles.delete} icon fa fa-times-circle`;
@@ -29,7 +38,7 @@ class ModuleWrapper extends React.Component {
     return (
       <div className={wrapperStyle}>
           <i className={deleteButton} onClick={this.handleDelete} aria-hidden="true"></i>
-        {React.createElement(component, {...this.props})}
+        {React.createElement(component, {...this.props, collapsed, handleCollapseFunction})}
       </div>
     )
   }
