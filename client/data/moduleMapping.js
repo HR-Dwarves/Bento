e25a4a32ad16a194a0049f8c1af4b76b88a8200d
Michael Tutt <tutt.michael@gmail.com>
2017-02-25 08:31:44 -0800

    Add moduleMapping to support older naming of modules

---
+++ client/data/moduleMapping.js
@@ -0,0 +1,21 @@
+import List from '../components/List/List';
+import NewsFeed from '../components/NewsFeed/NewsFeed.js';
+import WeatherDetails from '../components/WeatherDetails/WeatherDetails';
+import StickyNotes from '../components/StickyNotes/StickyNotes';
+import Modal from '../components/Modal/Modal';
+import LatLong from '../components/LatLong/LatLong';
+import DefaultModule from '../components/DefaultModule/DefaultModule'
+
+var moduleList = {
+  'List': List,
+  'NewsFeed': NewsFeed,
+  'Hacker News': NewsFeed,
+  'Weather': WeatherDetails,
+  'WeatherDetails': WeatherDetails,
+  'StickyNotes': StickyNotes,
+  'Sticky Note': StickyNotes,
+  'LatLong': LatLong,
+  'Clock': LatLong
+};
+
+export default moduleList;
