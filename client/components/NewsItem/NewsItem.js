51590c88734638e6db53292287005830b79e5955
joeylaguna <elagunaj@gmail.com>
2017-02-25 14:49:40 -0800

    Add css classes for news item and new link

--- client/components/NewsItem/NewsItem.js
+++ client/components/NewsItem/NewsItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './NewsItem.css';
 
 const NewsItem = React.createClass({
   render(){
   	const item = this.props.newsItem;
+  	const newsItem = `${styles.newsItem}`
+  	const newsLink = `${styles.newsLink}`
   	return(
-  	  <div className={'newsItem'}>
-  	    <a href={item.url ? item.url : 'https://news.ycombinator.com/item?id=' + item.id} target="_blank">{item.title}</a>
+  	  <div className={newsItem}>
+  	    <a className={newsLink} href={item.url ? item.url : 'https://news.ycombinator.com/item?id=' + item.id} target="_blank">{item.title}</a>
   	  </div>
   	)
   }
