a5df8bdb73ff79750f2b601b14f466569dd3e137
joeylaguna <elagunaj@gmail.com>
2017-02-25 14:43:25 -0800

    Add border bottom to news posts. Changed news posts links color

--- client/components/NewsItem/NewsItem.js
+++ client/components/NewsItem/NewsItem.js
@@ -4,7 +4,7 @@ const NewsItem = React.createClass({
   render(){
   	const item = this.props.newsItem;
   	return(
-  	  <div>
+  	  <div className={'newsItem'}>
   	    <a href={item.url ? item.url : 'https://news.ycombinator.com/item?id=' + item.id} target="_blank">{item.title}</a>
   	  </div>
   	)
