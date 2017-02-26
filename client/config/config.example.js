a7b22c5152f2f9d2243d8259c4d3b5d554ba23e4
Michael Tutt <tutt.michael@gmail.com>
2017-02-25 16:14:05 -0800

    Update config file to only include firebase

--- client/config/config.example.js
+++ client/config/config.example.js
@@ -1,9 +1,7 @@
 const config = {
   apiKey: 'ADD KEY HERE!',
   authDomain: 'ADD DOMAIN HERE!',
-  databaseURL: 'ADD URL HERE!',
-  openWeatherMapAPIKey: 'API KEY HERE',
-  googleApiKey: 'GEO LOCATION API KEY HERE'
+  databaseURL: 'ADD URL HERE!'
 };
 
 export default config;
---
+++ client/config/config.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: 'AIzaSyA2Vq9okaPvwNahEVH0s-KgyJ2fBattPOw',
+  authDomain: 'dashboardapp-3d3c7.firebaseapp.com',
+  databaseURL: 'https://dashboardapp-3d3c7.firebaseio.com'
+};
+
+export default config;
