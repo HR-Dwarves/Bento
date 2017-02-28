e4f78cc6caf17cc3ff74a91e3a2130b587ded74b
Benson Ninh <bensonninh@gmail.com>
2017-02-27 17:15:14 -0800

    add bubble bobble to the list of games

--- client/components/Arcade/Arcade.js
+++ client/components/Arcade/Arcade.js
@@ -16,7 +16,7 @@ class Arcade extends React.Component {
         'PACMAN': 'http://www.classicgamesarcade.com/games/pacman.swf',
         'SPACE INVADERS': 'http://www.pizn.com/swf/1-space-invaders.swf',
         'GALAGA': 'http://www.classicgamesarcade.com/games/galaga.swf',
-
+        'BUBBLE BOBBLE': 'http://www.classicgamesarcade.com/games/puzzle-bobble.swf'
       }
     }
   }
