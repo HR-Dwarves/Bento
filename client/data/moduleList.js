8cdbc27342ac7d087cbae60fd1a97e61c4de8e59
Benson Ninh <bensonninh@gmail.com>
2017-02-27 17:12:54 -0800

    add arcade component to the module list

--- client/data/moduleList.js
+++ client/data/moduleList.js
@@ -4,6 +4,7 @@ import WeatherDetails from '../components/WeatherDetails/WeatherDetails';
 import StickyNotes from '../components/StickyNotes/StickyNotes';
 import Modal from '../components/Modal/Modal';
 import LatLong from '../components/LatLong/LatLong';
+import Arcade from '../components/Arcade/Arcade';
 import DefaultModule from '../components/DefaultModule/DefaultModule'
 
 var moduleList = {
@@ -11,7 +12,8 @@ var moduleList = {
   'Hacker News': NewsFeed,
   'Weather': WeatherDetails,
   'Sticky Note': StickyNotes,
-  'Clock': LatLong
+  'Clock': LatLong,
+  'Arcade': Arcade,
 };
 
 export default moduleList;
--- client/data/moduleMapping.js
+++ client/data/moduleMapping.js
@@ -4,7 +4,8 @@ import WeatherDetails from '../components/WeatherDetails/WeatherDetails';
 import StickyNotes from '../components/StickyNotes/StickyNotes';
 import Modal from '../components/Modal/Modal';
 import LatLong from '../components/LatLong/LatLong';
-import DefaultModule from '../components/DefaultModule/DefaultModule'
+import Arcade from '../components/Arcade/Arcade';
+import DefaultModule from '../components/DefaultModule/DefaultModule';
 
 var moduleList = {
   'List': List,
@@ -15,7 +16,8 @@ var moduleList = {
   'StickyNotes': StickyNotes,
   'Sticky Note': StickyNotes,
   'LatLong': LatLong,
-  'Clock': LatLong
+  'Clock': LatLong,
+  'Arcade': Arcade,
 };
 
 export default moduleList;
