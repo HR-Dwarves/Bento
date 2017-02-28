6b14e5394f551f77e4fbada68aaa32dc07637322
Benson Ninh <bensonninh@gmail.com>
2017-02-28 15:38:03 -0800

    give the focus card an input field and text in the content of the card

--- client/components/Focus/Focus.js
+++ client/components/Focus/Focus.js
@@ -13,13 +13,42 @@ class Focus extends React.Component {
   render() {
     let dashboard = this.props.dashboard;
     let db_key = this.props.db_key;
-    let note = dashboard.modules[db_key];
+    let focus = dashboard.modules[db_key];
+
+    let collapsed = this.props.collapsed.collapsed;
+    let collapsedStyle = classnames(`${styles.height}`, collapsed ? `${styles.collapsedStyle}` : '');
+
+    let focusContentStyle = `${styles.focusStyle} 'card-content`;
 
     return (
-      <div>
-        <p>
-          FOCUS
-        </p>
+      <div className='focus'>
+        <div className='card'>
+          <header className='card-header'>
+            <div className='card-header-title'>
+              <p>FOCUS</p>
+            </div>
+            <div className="card-header-icon">
+              <span className="icon">
+                <i onClick={this.props.handleCollapseFunction} className="fa fa-crosshairs" aria-hidden="true"></i>
+              </span>
+            </div>
+          </header>
+          <div className={collapsedStyle}>
+            <div className='card-content'>
+              <div className={styles.focusStyle}>
+                <div>
+                  <p>WHAT'S YOUR FOCUS?</p>
+                </div>
+              </div>
+              <span className={styles.focusContent}>
+                <input className={styles.focusInput}
+                        type='text'
+                        maxLength='40'
+                      />
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
