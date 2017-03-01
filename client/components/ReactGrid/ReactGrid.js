d03fe328d277f34af59c6b072df625509bf435d5
Michael Tutt <tutt.michael@gmail.com>
2017-03-01 12:07:17 -0800

    Update grid with new styling

--- client/components/ReactGrid/ReactGrid.js
+++ client/components/ReactGrid/ReactGrid.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Responsive, WidthProvider} from 'react-grid-layout';
+import { Responsive, WidthProvider } from '../../../node_modules/react-grid-layout';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 import firebaseApp from '../../base';
@@ -12,7 +12,7 @@ import styles from './ReactGrid.css';
 import gridStyles from '../../../node_modules/react-grid-layout/css/styles.css';
 import resizableStyles from '../../../node_modules/react-resizable/css/styles.css';
 
-var datagrid = {x: 0, y: 0, w: 3, h: 1};
+// var datagrid = {x: 0, y: 0, w: 3, h: 1};
 
 
 class ReactGrid extends React.PureComponent {
@@ -20,7 +20,8 @@ class ReactGrid extends React.PureComponent {
     super();
     this.state = {
       layouts: {},
-      currentBreakpoint: 'lg',
+      breakpoint: 'lg',
+      cols: 12,
       mounted: false
     }
     this.onBreakpointChange = this.onBreakpointChange.bind(this);
@@ -28,8 +29,8 @@ class ReactGrid extends React.PureComponent {
   }
 
   componentDidMount() {
-     this.setState({mounted: true});
-   }
+    this.setState({mounted: true});
+  }
   // Returns initial state from local storage
   getInitialState() {
     return {
@@ -55,16 +56,17 @@ class ReactGrid extends React.PureComponent {
     }
   }
 
-  onBreakpointChange(breakpoint) {
+  onBreakpointChange(breakpoint, columns) {
     this.setState({
-      currentBreakpoint: breakpoint
+      breakpoint: breakpoint,
+      cols: columns
     });
   };
 
   onLayoutChange(layout, layouts) {
     this.setState({
       layout: layout
-    })
+    });
   }
 
   generateLayout(key) {
@@ -73,58 +75,67 @@ class ReactGrid extends React.PureComponent {
 
   render() {
     let dashboard = this.props.dashboard;
-    let modules, wrappers;
+    let modules, wrappers, gridProps, gridItems;
 
     if (dashboard) {
       modules = dashboard.modules
       if (modules) {
         wrappers = Object.keys(modules).map((moduleKey, ind, array) => {
+          
           var additionalProps = {
             key: moduleKey,
             db_key: moduleKey,
             type: modules[moduleKey].type
-          }
-          var gridProps = {
-            i: ind.toString(),
-            x: ind * 2, 
-            y: 0, 
-            w: 3, 
-            h: 3, 
-            add: ind === (array.length - 1).toString()
           };
-          var newProps = Object.assign({}, this.props, additionalProps, gridProps)
+
+          
+          var newProps = Object.assign({}, this.props, additionalProps);
           return React.createElement(ModuleWrapper, newProps);
         });
+
+        gridItems = wrappers.map((wrapper, ind, arr) => {
+          gridProps = {
+            i: ind.toString(),
+            x: ind * 3, 
+            y: 0, 
+            w: 3, 
+            h: 3,
+            minW: 3,
+            add: ind === (arr.length - 1).toString()
+          };
+
+          return (<div key={ind} data-grid={gridProps}>{wrapper}</div>);
+        })
       }
     }
     let defaultModule = <div className={componentStyle}><DefaultModule key={'abcd'}/></div>;
 
     // layout is an array of objects, see the demo for more complete usage
-    var layoutStyle = `${styles.layout} layout`
-    let componentStyle = `${styles.component} animated`;
+    var layoutStyle = `${styles.layout} layout ${gridStyles} ${resizableStyles}`;
+    let componentStyle = `${styles.component}`;
 
     return (
       <ResponsiveReactGridLayout 
       {...this.props}
       ref='rrgl'
       className={layoutStyle} 
-      // layouts={this.state.layouts} 
       breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
       measureBeforeMount={false}
       onBreakpointChange={this.onBreakpointChange}
       useCSSTransforms={this.state.mounted}
       >
-      {wrappers ? wrappers : defaultModule }
+        {gridItems ? gridItems : defaultModule }
       </ResponsiveReactGridLayout>
-    )
+    );
   }
-};
+}
 
 ReactGrid.defaultProps = {
   className: "layout",
   cols: {lg: 12, md: 9, sm: 6, xs: 3, xxs: 3},
   autoSize: true,
   rowHeight: 100,
+  minH: 3,
   isResizable: true,
   isDraggable: true,
   margin: [20, 20],
