1a4088c8ef52bd241be8df82e81979fa107e6a36
Michael Tutt <tutt.michael@gmail.com>
2017-03-06 11:09:56 -0800

    Update default grid props for larger minW List size

--- client/components/ReactGrid/defaultGridProps.js
+++ client/components/ReactGrid/defaultGridProps.js
@@ -1,6 +1,6 @@
 
 var defaultGridProps = {
-  'List': { minW: 1, minH: 2, isResizable: true },
+  'List': { minW: 2, minH: 2, isResizable: true },
   'Sticky Note': { minW: 2, minH: 2, isResizable: true },
   'Default': { w: 3, h: 2, x: 0, y: Infinity, minW: 3, minH: 2 },
   'Weather': { minW: 3, minH: 2, isResizable: true },
