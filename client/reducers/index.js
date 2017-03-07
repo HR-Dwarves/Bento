86e123a35f48cb9cc6881ae20515af14fb6bd045
Michael Tutt <tutt.michael@gmail.com>
2017-03-07 15:08:19 -0800

    Add notification reducer to root reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -7,8 +7,9 @@ import list from './list';
 import newsfeed from './newsfeed';
 import authentication from './authentication';
 import layouts from './layouts';
+import notification from './notification';
 
 // pass reducers into combineReducers (in first object)
-const rootReducer = combineReducers({dashboard, list, newsfeed, authentication, layouts, routing: routerReducer });
+const rootReducer = combineReducers({dashboard, list, newsfeed, authentication, layouts, notification, routing: routerReducer });
 
 export default rootReducer;
