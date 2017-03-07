fcd2ed9943616b44bc6c07f8a2da7f652ece7be7
Michael Tutt <tutt.michael@gmail.com>
2017-03-07 15:07:52 -0800

    Remove unused Text from Signup page

--- client/components/Signup/Signup.js
+++ client/components/Signup/Signup.js
@@ -81,7 +81,6 @@ class Signup extends React.Component {
       return (
         <nav className={styles.signup}>
           <div className={styles.signupHeader}>
-            <span>SIGNUP</span>
           </div>
           <div className={styles.loginButtons}>
             <button className={googleStyle} onClick={() => this.authenticate('google')}>
