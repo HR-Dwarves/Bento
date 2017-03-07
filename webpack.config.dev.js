339845a7237a56ed977610277f189f8b7b109ea6
Michael Tutt <tutt.michael@gmail.com>
2017-03-06 17:07:27 -0800

    add postcss-loader to webpack dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -27,7 +27,7 @@ module.exports = {
       // CSS
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+        loader: 'style-loader!css-loader?modules&localIdentName=[name]---[local]---[hash:base64:5]!postcss-loader'
       }
     ]
   },
