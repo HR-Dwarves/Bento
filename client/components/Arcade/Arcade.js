3ba9f3f07216aba5c651f0697db9546a68329ff5
Benson Ninh <bensonninh@gmail.com>
2017-03-08 11:04:33 -0800

    move list of games to an external file

--- client/components/Arcade/Arcade.js
+++ client/components/Arcade/Arcade.js
@@ -5,21 +5,22 @@ import classnames from 'classnames';
 const database = firebaseApp.database();
 
 import styles from './Arcade.css';
+import games from './listOfGames.js';
 
 class Arcade extends React.Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
       'currentGame': 'PACMAN',
       'games': {
-        'BUBBLE BOBBLE': 'https://firebasestorage.googleapis.com/v0/b/dashboardapp-3d3c7.appspot.com/o/arcade-games%2Fpuzzle-bobble.swf?alt=media&token=f1cbbec5-a12f-4e30-a300-d0b4d2a4353e',
-        'GALAGA': 'https://firebasestorage.googleapis.com/v0/b/dashboardapp-3d3c7.appspot.com/o/arcade-games%2Fgalaga.swf?alt=media&token=34785cdd-d849-4127-b96e-99ac120f8799',
-        'PACMAN': 'https://firebasestorage.googleapis.com/v0/b/dashboardapp-3d3c7.appspot.com/o/arcade-games%2Fpacman.swf?alt=media&token=f252033a-42ac-47d9-82b0-80d8fb29d361',
-        'RAIDENX': 'https://firebasestorage.googleapis.com/v0/b/dashboardapp-3d3c7.appspot.com/o/arcade-games%2Fraidenx.swf?alt=media&token=876f1bb8-fd14-4b16-9e6e-6e110ecedd7c',
-        'SPACE INVADERS': 'https://firebasestorage.googleapis.com/v0/b/dashboardapp-3d3c7.appspot.com/o/arcade-games%2F1-space-invaders.swf?alt=media&token=b439d968-d7cd-4bbb-8c82-11cc0cc1c647',
-        'TRON': 'https://firebasestorage.googleapis.com/v0/b/dashboardapp-3d3c7.appspot.com/o/arcade-games%2Ftron.swf?alt=media&token=92bd7332-7f39-4195-a349-f024833b3b8f',
-        'TETRIS': 'https://firebasestorage.googleapis.com/v0/b/dashboardapp-3d3c7.appspot.com/o/arcade-games%2Fflash-tetris.swf?alt=media&token=a11c7ce2-256e-4ce2-8090-741a65ea3c28',
+        'BUBBLE BOBBLE': games.BUBBLE_BOBBLE,
+        'GALAGA': games.GALAGA,
+        'PACMAN': games.PACMAN,
+        'RAIDENX': games.RAIDENX,
+        'SPACE INVADERS': games.SPACE_INVADERS,
+        'TRON': games.TRON,
+        'TETRIS': games.TETRIS,
       }
     }
 
