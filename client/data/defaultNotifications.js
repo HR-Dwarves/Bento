0453ffe564170d23a9e1838609557246ffb72f0e
Michael Tutt <tutt.michael@gmail.com>
2017-03-07 21:03:50 -0800

    Add default notifications file for the addition of new cards

---
+++ client/data/defaultNotifications.js
@@ -0,0 +1,7 @@
+
+var defaultNotifications = {
+  'List': {text: 'List', dismiss: false, type: 'List', timeout: 3000},
+  'Default': {text: 'DEFAULT', dismiss: false, type: 'Other', timeout: 3000}
+}
+
+export default defaultNotifications;
