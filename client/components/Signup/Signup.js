1aa84024a62fdadd82e44c44ad67badd51edb535
joeylaguna <elagunaj@gmail.com>
2017-03-08 14:44:31 -0800

    Signup page work in progress

--- client/components/Signup/Signup.js
+++ client/components/Signup/Signup.js
@@ -99,77 +99,77 @@ class Signup extends React.Component {
     if (!this.state.authInProcess) {
       return (
         <div>
-        <div className='columns'>
-          <section className={splash}>
-            <div className={styles.bentoMainSplashContent}>
-              <h1 className={styles.splashHeader}>Bento</h1>
-              <p className={styles.splashSubHeader}>Your own customizable productivity dashboard. Login below.</p>
-              <br/>
-              <div className={styles.loginButtons}>
-                <button className={googleStyle} onClick={() => this.authenticate('google')}>
-                  <span className={styles.buttonText}>
-                    Google
-                  </span>
-                  <span className="icon">
-                    <i className={googleIconStyle} aria-hidden="true"></i>
-                  </span>
-                </button>
-                <button className={githubStyle} onClick={() => this.authenticate('github')}>
-                  <span className={styles.buttonText}>
-                    Github
-                  </span>
-                  <span className="icon">
-                    <i className="fa fa-github" aria-hidden="true"></i>
-                  </span>
-                </button>
-                <button className={facebookStyle} onClick={() => this.authenticate('facebook')}>
-                  <span className={styles.buttonText}>
-                    Facebook
-                  </span>
-                  <span className="icon">
-                    <i className="fa fa-facebook-official" aria-hidden="true"></i>
-                  </span>
-                </button>
-                <button className={twitterStyle} onClick={() => this.authenticate('twitter')}>
-                  <span className={styles.buttonText}>
-                    Twitter
-                  </span>
-                  <span className="icon">
-                    <i className="fa fa-twitter" aria-hidden="true"></i>
-                  </span>
-                </button>
+          <div className='columns'>
+            <section className={splash}>
+              <div className={styles.bentoMainSplashContent}>
+                <h1 className={styles.splashHeader}>Bento</h1>
+                <p className={styles.splashSubHeader}>Your own customizable productivity dashboard. Login below.</p>
+                <br/>
+                <div className={styles.loginButtons}>
+                  <button className={googleStyle} onClick={() => this.authenticate('google')}>
+                    <span className={styles.buttonText}>
+                      Google
+                    </span>
+                    <span className="icon">
+                      <i className={googleIconStyle} aria-hidden="true"></i>
+                    </span>
+                  </button>
+                  <button className={githubStyle} onClick={() => this.authenticate('github')}>
+                    <span className={styles.buttonText}>
+                      Github
+                    </span>
+                    <span className="icon">
+                      <i className="fa fa-github" aria-hidden="true"></i>
+                    </span>
+                  </button>
+                  <button className={facebookStyle} onClick={() => this.authenticate('facebook')}>
+                    <span className={styles.buttonText}>
+                      Facebook
+                    </span>
+                    <span className="icon">
+                      <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                    </span>
+                  </button>
+                  <button className={twitterStyle} onClick={() => this.authenticate('twitter')}>
+                    <span className={styles.buttonText}>
+                      Twitter
+                    </span>
+                    <span className="icon">
+                      <i className="fa fa-twitter" aria-hidden="true"></i>
+                    </span>
+                  </button>
+                  </div>
                 </div>
+              </section>
+          </div>
+          <div className='columns'>
+            <div className={aboutBento}>
+              <div className={styles.aboutText}>
+                <h1 className={styles.aboutHeader}>What is Bento?</h1>
+                <p className={styles.aboutSubHeader}>Bento is a fully customizable productivity dashboard that provides you with a dashboard that you cater to your needs.</p>
               </div>
-            </section>
-        </div>
-        <div className='columns'>
-          <div className={aboutBento}>
-            <div className={styles.aboutText}>
-              <h1 className={styles.aboutHeader}>What is Bento?</h1>
-              <p className={styles.aboutSubHeader}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </div>
-        </div>
-        <div className='columns'>
-            <div className={iconStyle}>
-              <img src='http://www.languagenut.com/assets/media/placeholders/250x250-circle.png'/>
-              <h1>1</h1>
-              <p>Login</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <div className='columns'>
+              <div className={iconStyle}>
+                <img src='http://www.languagenut.com/assets/media/placeholders/250x250-circle.png'/>
+                <h1>1</h1>
+                <p>Login</p>
+                <p>Login using your favorite social media account</p>
+              </div>
+              <div className={iconStyle}>
+                <img src='http://www.languagenut.com/assets/media/placeholders/250x250-circle.png'/>
+                <h1>2</h1>
+                <p>Add modules</p>
+                <p>Add as many modules as you like</p>
+              </div>
+              <div className={iconStyle}>
+                <img src='http://www.languagenut.com/assets/media/placeholders/250x250-circle.png'/>
+                <h1>3</h1>
+                <p>Customize it!</p>
+                <p>Drag and drop to customize your dashboard</p>
+              </div>
             </div>
-            <div className={iconStyle}>
-              <img src='http://www.languagenut.com/assets/media/placeholders/250x250-circle.png'/>
-              <h1>2</h1>
-              <p>Add modules</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div className={iconStyle}>
-              <img src='http://www.languagenut.com/assets/media/placeholders/250x250-circle.png'/>
-              <h1>3</h1>
-              <p>Customize it!</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
         </div>
       );
     } else {
