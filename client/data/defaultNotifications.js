fa50ad7c5b3b6c23d5df18f157008793596ec762
Michael Tutt <tutt.michael@gmail.com>
2017-03-08 10:07:17 -0800

    Change reference to moduleType from type

--- client/data/defaultNotifications.js
+++ client/data/defaultNotifications.js
@@ -1,7 +1,7 @@
 
 var defaultNotifications = {
-  'List': {text: 'List', dismiss: false, type: 'List', timeout: 3000},
-  'Default': {text: 'DEFAULT', dismiss: false, type: 'Other', timeout: 3000}
+  'List': {text: 'List', dismiss: false, moduleType: 'List', timeout: 3000},
+  'Default': {text: 'DEFAULT', dismiss: false, moduleType: 'Other', timeout: 3000}
 }
 
 export default defaultNotifications;
