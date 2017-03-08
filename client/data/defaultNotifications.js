a41fef4d4f4434f3433e652f5c3b96c54d9f1440
Michael Tutt <tutt.michael@gmail.com>
2017-03-08 14:56:36 -0800

    Add notification for no cards situation

--- client/data/defaultNotifications.js
+++ client/data/defaultNotifications.js
@@ -4,10 +4,32 @@ const listHelp = `Use the list card to stay focused on your priorities!
 \nDelete list items using the "Edit" button and clicking on the "X" icon.
 `;
 
+const stickyNoteHelp = `Sticky Note Card
+\n
+`;
+
+const newsHelp = `News Card
+\n
+`;
+
+const weatherHelp = `Weather Card
+\n
+`;
+
+const focusHelp = `Use the Focus card to set your daily intentions!
+\n
+`;
+
+const defaultHelp = `Add cards using the button above to get started!
+`;
 
 var defaultNotifications = {
   'List': {text: listHelp, dismiss: false, moduleType: 'List', timeout: 3000},
-  'Default': {text: 'DEFAULT', dismiss: false, moduleType: 'Other', timeout: 3000}
+  'Sticky Note': {text: stickyNoteHelp, dismiss: false, moduleType: 'Sticky Note', timeout: 3000},
+  'Hacker News': {text: newsHelp, dismiss: false, moduleType: 'Hacker News', timeout: 3000},
+  'Weather': {text: weatherHelp, dismiss: false, moduleType: 'Weather', timeout: 3000},
+  'Focus': {text: focusHelp, dismiss: false, moduleType: 'Focus', timeout: 3000},
+  'Default': {text: defaultHelp, dismiss: false, moduleType: 'Other', timeout: 3000}
 }
 
 export default defaultNotifications;
