3d2adf532f8722bacbae0ee0c860e30e51f45189
Michael Tutt <tutt.michael@gmail.com>
2017-03-07 20:57:18 -0800

    Udpdate notification action creators for add / remove

--- client/actions/notificationActions.js
+++ client/actions/notificationActions.js
@@ -2,15 +2,29 @@ import ActionTypes from './actionTypes';
 import firebaseApp from '../base';
 const database = firebaseApp.database();
 
-export function addNotification(index, user = 'testUser') {
+export function addNotification(message, user = 'testUser') {
   return dispatch => {
-    dispatch()
+    dispatch(addNotificationAction(message));
   }
 }
 
 
 export function removeNotification(index, user = 'testUser') {
   return dispatch => {
-    dispatch()
+    dispatch(removeNotificationAction(index));
+  }
+}
+
+function addNotificationAction(message) {
+  return {
+    type: ActionTypes.AddNotification,
+    message
+  }
+}
+
+function removeNotificationAction(index) {
+  return {
+    type: ActionTypes.RemoveNotification,
+    index
   }
 }
