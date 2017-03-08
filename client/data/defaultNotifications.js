3723342ebdb45ef29e5d04feb6d0841d352c68b0
Michael Tutt <tutt.michael@gmail.com>
2017-03-08 10:50:09 -0800

    Update list notification

--- client/data/defaultNotifications.js
+++ client/data/defaultNotifications.js
@@ -1,6 +1,8 @@
+const listHelp = `Use the list card to stay focused on your priorities! \nAdd new items using the input field and pressing 'Enter' or by using the 'Add' button.`;
+
 
 var defaultNotifications = {
-  'List': {text: 'List', dismiss: false, moduleType: 'List', timeout: 3000},
+  'List': {text: listHelp, dismiss: false, moduleType: 'List', timeout: 3000},
   'Default': {text: 'DEFAULT', dismiss: false, moduleType: 'Other', timeout: 3000}
 }
 
