1c685ff1da2969530877f6c7cae2e51ee20fc8a9
Michael Tutt <tutt.michael@gmail.com>
2017-03-08 11:35:30 -0800

    Complete default List notification

--- client/data/defaultNotifications.js
+++ client/data/defaultNotifications.js
@@ -1,4 +1,8 @@
-const listHelp = `Use the list card to stay focused on your priorities! \nAdd new items using the input field and pressing 'Enter' or by using the 'Add' button.`;
+const listHelp = `Use the list card to stay focused on your priorities!
+\nAdd new items using the input field and pressing "Enter" or by using the "Add" button.
+\nMark items completed by clicking the box to the right of each item. Hide completed items using the "Hide" button. Reveal the completed items by using the "Show" button.
+\nDelete list items using the "Edit" button and clicking on the "X" icon.
+`;
 
 
 var defaultNotifications = {
