c2cde089ebbba1c6673f32dcbe2273d054b85c83
Michael Tutt <tutt.michael@gmail.com>
2017-03-08 22:05:30 -0800

    Update logic for displaying default module notification

--- client/components/DefaultModule/DefaultModule.js
+++ client/components/DefaultModule/DefaultModule.js
@@ -5,18 +5,17 @@ import defaultNotifications from '../../data/defaultNotifications';
 
 
 class DefaultModule extends React.Component{
+  constructor() {
+    super();
+  }
 
   componentDidMount() {
-    if (!this.props.dashboard.modules) {
-      let notification = defaultNotifications['Default'];
-      let newNotification = Object.assign({}, notification);
-      this.props.addNotification(newNotification);
-    }
+    let notification = defaultNotifications['Default'];
+    let newNotification = Object.assign({}, notification);
+    this.props.addNotification(newNotification);
   }
 
   render() {
-    let defaultStyle = `${styles.defaultCard} card`;
-
   	return(
   	  <div>
   	  </div>
