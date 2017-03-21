cbb661b7304f6b69d620179e9d874ef4599ed6f1
Michael Tutt <tutt.michael@gmail.com>
2017-03-21 15:15:13 -0700

    Refactor signup to use AuthProvider component / remove unused styling

--- client/components/Signup/Signup.js
+++ client/components/Signup/Signup.js
@@ -2,10 +2,11 @@ import React from 'react';
 import firebaseApp from '../../base';
 import styles from './Signup.css';
 import authProviders from '../../authProviders';
+import AuthProvider from '../AuthProvider/AuthProvider';
+import Loading from '../Loading/Loading';
 
 const database = firebaseApp.database();
 
-import Loading from '../Loading/Loading';
 
 class Signup extends React.Component {
   constructor() {
@@ -15,27 +16,27 @@ class Signup extends React.Component {
       authInProcess: false
     }
     this.authenticate = this.authenticate.bind(this);
-    this.handleLoginModal = this.handleLoginModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
   }
 
+  // Authenticate function to be passed down to AuthProvider
+    // Allows user to login using major providers via Firebase
   authenticate(provider){
     var context = this;
     var authProvider = authProviders[provider];
+
+    // Set authInProcess to true and load the loading animation
     this.setState({
       authInProcess: true
     })
 
     firebaseApp.auth().signInWithPopup(authProvider)
     .then(function(result) {
-      context.setState({
-        authInProcess: false
-      })
       context.props.authenticateUser(result);
       let { displayName, uid, email, photoURL } = result.user;
       let userRef = database.ref(`users/${uid}`);
       userRef.once('value', snap => {
         let exists = snap.exists();
+        // If login is successful and user does not exist, create account and redirect to home page
         if (!exists) {
           let initData = Object.assign({}, {displayName, uid, email, photoURL})
           userRef.set(initData, function() {
@@ -43,13 +44,13 @@ class Signup extends React.Component {
             context.props.router.push('/');
           });
         } else {
-          // Redirect user to root page
+          // Redirect user to root page if account already exists
           context.props.router.push('/');
         }
       })
     })
     .catch(function(err) {
-      alert('Login error, please try and login again');
+      alert('Login error, please try and login again!');
       console.error(err);
       context.setState({
         authInProcess: false
@@ -57,35 +58,15 @@ class Signup extends React.Component {
     })
   }
 
-  handleLoginModal(){
-    this.setState({
-      clicked: !this.state.clicked
-    });
-  }
-
-  closeModal(){
-    this.setState({
-      clicked: false
-    });
-  }
-
   render() {
-    let googleStyle = `${styles.google} button ${styles.loginButton}`;
-    let githubStyle = `${styles.github} button ${styles.loginButton}`;
-    let facebookStyle = `${styles.facebook} button ${styles.loginButton}`;
-    let twitterStyle = `${styles.twitter} button ${styles.loginButton}`;
-    let googleIconStyle = `${styles.loginIcon} fa fa-google`;
-    let githubIconStyle = `${styles.loginIcon} fa fa-github`;
-    let facebookIconStyle = `${styles.loginIcon} fa fa-facebook`;
-    let twitterIconStyle = `${styles.loginIcon} fa fa-twitter`;
-    let getStartedButton = `${styles.getStartedButton} button is-outlined`;
+
+    // Pull styles in from CSS module
     let loader = `${styles.loader}`;
     let splash = `${styles.bentoMainSplash} column`;
     let aboutBento = `${styles.aboutBento} column`;
     let iconSection = `${styles.iconSection} column`;
     let iconStyle = `${styles.iconStyle} column`;
-    let loginButton = `${styles.loginButton}`;
-    let iconSpan = `${styles.iconSpan} icon`;
+
 
     if (!this.state.authInProcess) {
       return (
@@ -96,39 +77,13 @@ class Signup extends React.Component {
                 <h1 className={styles.splashHeader}>Bento</h1>
                 <p className={styles.splashSubHeader}>Revitalizing productivity</p>
                 <br/>
+                <div>Please login using your preferred provider below</div>
                 <div className={styles.loginButtons}>
-                  <button className={googleStyle} onClick={() => this.authenticate('google')}>
-                    <span className={styles.buttonText}>
-                      Google
-                    </span>
-                    <span className={iconSpan}>
-                      <i className={googleIconStyle} aria-hidden="true"></i>
-                    </span>
-                  </button>
-                  <button className={githubStyle} onClick={() => this.authenticate('github')}>
-                    <span className={styles.buttonText}>
-                      Github
-                    </span>
-                    <span className={iconSpan}>
-                      <i className="fa fa-github" aria-hidden="true"></i>
-                    </span>
-                  </button>
-                  <button className={facebookStyle} onClick={() => this.authenticate('facebook')}>
-                    <span className={styles.buttonText}>
-                      Facebook
-                    </span>
-                    <span className={iconSpan}>
-                      <i className="fa fa-facebook-official" aria-hidden="true"></i>
-                    </span>
-                  </button>
-                  <button className={twitterStyle} onClick={() => this.authenticate('twitter')}>
-                    <span className={styles.buttonText}>
-                      Twitter
-                    </span>
-                    <span className={iconSpan}>
-                      <i className="fa fa-twitter" aria-hidden="true"></i>
-                    </span>
-                  </button>
+                  {Object.keys(authProviders).map(provider => {
+                    return <AuthProvider 
+                            authenticate={this.authenticate}
+                            provider={provider} />
+                  })}
                   </div>
                 </div>
                 <div>***Please disable popup blockers before logging in</div>
