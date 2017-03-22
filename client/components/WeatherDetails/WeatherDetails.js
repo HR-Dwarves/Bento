55a8bae6e20f7e2758dc6214461ec385bfd8255a
Michael Tutt <tutt.michael@gmail.com>
2017-03-21 17:55:10 -0700

    Make weather icon and temp numbers stacked instead of side by side (fixing compacted text on larger screens)

--- client/components/WeatherDetails/WeatherDetails.js
+++ client/components/WeatherDetails/WeatherDetails.js
@@ -157,10 +157,8 @@ class WeatherDetails extends React.Component {
                       <div className={styles.forecastDetail}>
                         <p className={styles.forecastDay}>{details.day}</p>
                         <p className={styles.forecastDate}>{details.date.substring(3, 6)} {details.date.substring(0, 2)}</p>
-                        <div className={styles.forecastIconTemp}>
-                          <i className={forecastIcon(details.code)}></i>
-                          <p className={styles.forecastTemp}>{Math.floor((parseInt(details.high) + parseInt(details.low))/ 2)}º</p>
-                        </div>
+                        <p className={styles.forecastTemp}>{Math.floor((parseInt(details.high) + parseInt(details.low))/ 2)}º</p>
+                        <i className={forecastIcon(details.code)}></i>
                         <p className={styles.forecastDate}>{details.text}</p>
                       </div>
                     );
